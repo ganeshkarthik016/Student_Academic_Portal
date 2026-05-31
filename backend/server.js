1368a0d093ea1d7ecfab4a53e492826561243b66
Ganesh Karthik <ganeshkarthik2007.gk@gmail.com>
2026-06-01 01:12:58 +0530

    Fix render port

--- backend/server.js
+++ backend/server.js
@@ -193,7 +193,7 @@ app.get('/api/all-electives/:rollNumber', (req, res) => {
         res.json(results);
     });
 });
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 Backend Server is running on http://localhost:${PORT}`);
 });
